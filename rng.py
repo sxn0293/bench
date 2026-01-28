19b8b6b29e4a16e8e78971af3e8cae3e7c695150
sxn0293 <sxn0293@mavs.uta.edu>
2026-01-28 15:13:22 -0600

    format

--- rng.py
+++ rng.py
@@ -1,9 +1,11 @@
 import random
+
 filename = "input.txt"
 count = 12345
 min_val = 1
 max_val = 1337
 with open(filename, "w") as file:
     file.write(f"{count} {random.randint(min_val, max_val)}\n")
-    for _ in range(count): file.write(f"{random.randint(min_val, max_val)} ")
+    for _ in range(count):
+        file.write(f"{random.randint(min_val, max_val)} ")
 print(f"{count} random integers between {min_val} and {max_val} written in {filename}")
